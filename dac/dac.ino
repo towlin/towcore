9e9628a97ad30a11f1354add167aa712593deef1
jonas <jonas.kazlauskas@me.com>
2022-02-01 11:46:48 -0500

    Add ability to change potentiometer mapping range

--- dac/dac.ino
+++ dac/dac.ino
@@ -1,22 +1,27 @@
 #include <Wire.h>
-//MCP4725 library
 #include <Adafruit_MCP4725.h>
 
 int potPort = A0;
+int minOut = 1550;
+int maxOut = 3100;
+int potMax = 1023;
+// calculate x value in y=mx + b where b = minOut, assumes potMin is 0
+float x = (float)(maxOut-minOut)/potMax;
 
 Adafruit_MCP4725 dac;
 
 void setup() {
-  //Start Serial
-  Serial.begin(9600);
+  Serial.begin(115200);
   //Start DAC. DAC address 0x60 or 0x61
   dac.begin(0x60);
 }
 
 int normPot(){
+  // Map the potentiometer value to minOut - maxOut scale
   int potRaw = analogRead(potPort);
-  return (int)(2.48*potRaw + 655);
+  return (int)(x*potRaw + minOut);
 }
+
 
 void loop(){
   Serial.println(normPot());
